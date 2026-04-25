9b624acfa82b65f1c51ec1018d8dec32e71b46f5
Lorena-Tonguino <lorena.sql.2000@gmail.com>
2026-04-25 01:16:09 +0000

    Actividad 1: Corrección de ruta de validación

--- text_to_columns_validator.py
+++ text_to_columns_validator.py
@@ -53,7 +53,7 @@ schema = T.StructType([T.StructField(f, T.StringType()) for f in fields])
 # 3️⃣ Ground Truth Record
 # ------------------------------------------------------------------------------
 truth_record = {
-    "source_file": "dbfs:/Volumes/logistics/bronze/raw/txt/source=UTB/1637351047899_UTB%20ME-FL-FL.txt",
+    "source_file": "/Volumes/logistics/bronze/raw/pdf/source=UTB/1637351047899_UTB ME-FL-FL.pdf,
     "broker_name": "USA Truck Brokers Inc.",
     "broker_phone": "305-819-3000",
     "broker_fax": "305-819-7146",
